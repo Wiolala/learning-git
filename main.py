6bebb48832f15888e3ae9894e8a1d1ac8361d25d
Wiolala <69370036+Wiolala@users.noreply.github.com>
2020-08-11 17:19:36 +0000

    Add comment

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 print("Witaj w świecie GitHuba!")
 print("'Hiszpańska inkwizycja' to najlepszy skecz Monty Pythona.")
+#Nie wiem jaki jest najlepszy skecz Monty Pythona, więc nie podważam powyższego zdania.
